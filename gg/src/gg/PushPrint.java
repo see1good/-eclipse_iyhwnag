33d5bc443315925ebb391f60bddc422648c5d452
K <see1good@naver.com>
2020-11-17 19:49:30 +0900

    Update PushPrint.java

--- gg/src/gg/PushPrint.java
+++ gg/src/gg/PushPrint.java
@@ -5,11 +5,9 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("sophomore");
-<<<<<<< HEAD
 		System.out.print("Wankwang Univ.");
-=======
 		System.out.print("Jeonbuk Iksan");
->>>>>>> refs/remotes/origin/master
+		System.out.print("fighting!");
 	}
 
 }
