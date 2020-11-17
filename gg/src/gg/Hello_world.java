a366f7558a5ab621e53223a4fff32a7284f0eaca
HIY <see1good@naver.com>
2020-11-17 19:05:36 +0900

    4

--- gg/src/gg/Hello_world.java
+++ gg/src/gg/Hello_world.java
@@ -5,6 +5,8 @@ public class Hello_world {
 	public static void main(String[] args) {
 		// TODO Auto-generated method
 		System.out.print("Hello_World");
+		System.out.print("황인영");
+		System.out.print("컴퓨터소프트웨어계열");
 	}
 
 }
